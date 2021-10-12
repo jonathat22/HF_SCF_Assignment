4b0eeaf1433eab25f2d67430a36de1405645a0e1
Jonathan Taylor <jonathat@andrew.cmu.edu>
2021-10-12 13:06:52 -0400

    Test_SCF_corrections

--- tests/SCF/test_SCF.py
+++ tests/SCF/test_SCF.py
@@ -5,9 +5,9 @@ from mol import mol
 
 
 Suv = pickle.load(open("suv.pkl", "rb"))
-Tuv = pickle.load(open("real_tuv.pkl", "rb"))
-eri = pickle.load(open("real_eri.pkl", "rb"))
-Vuv = pickle.load(open("real_vuv.pkl", "rb"))
+Tuv = pickle.load(open("tuv.pkl", "rb"))
+eri = pickle.load(open("eri.pkl", "rb"))
+Vuv = pickle.load(open("vuv.pkl", "rb"))
 
 
 def test_calc_nuclear_repulsion_energy(mol_h2o):
@@ -23,6 +23,8 @@ def test_calc_initial_density(mol_h2o):
 
 
 def test_calc_hcore_matrix(mol_h2o):
+    Tuv = pickle.load(open("tuv.pkl", "rb"))
+    Vuv = pickle.load(open("vuv.pkl", "rb"))
     assert SCF.calc_hcore_matrix(Tuv, Vuv)[0, 0] == -32.57739541261037
     assert SCF.calc_hcore_matrix(Tuv, Vuv)[3, 4] == 0.0
     assert SCF.calc_hcore_matrix(Tuv, Vuv)[4, 3] == 0.0
@@ -30,6 +32,9 @@ def test_calc_hcore_matrix(mol_h2o):
 
 
 def test_calc_fock_matrix(mol_h2o):
+    Tuv = pickle.load(open("tuv.pkl", "rb"))
+    Vuv = pickle.load(open("vuv.pkl", "rb"))
+    eri = pickle.load(open("eri.pkl", "rb"))
     Duv = SCF.calc_initial_density(mol_h2o)
     h_core = SCF.calc_hcore_matrix(Tuv, Vuv)
 
@@ -45,6 +50,10 @@ def test_calc_fock_matrix(mol_h2o):
 
 
 def test_solve_Roothan_equations(mol_h2o):
+    Tuv = pickle.load(open("tuv.pkl", "rb"))
+    Vuv = pickle.load(open("vuv.pkl", "rb"))
+    eri = pickle.load(open("eri.pkl", "rb"))
+    Suv = pickle.load(open("suv.pkl", "rb"))
     Duv = SCF.calc_initial_density(mol_h2o)
     h_core = SCF.calc_hcore_matrix(Tuv, Vuv)
     Fuv = SCF.calc_fock_matrix(mol_h2o, h_core, eri, Duv)
@@ -54,17 +63,21 @@ def test_solve_Roothan_equations(mol_h2o):
                                         -7.55008599, -7.36396923,
                                         -7.34714487, -4.00229867, -3.98111115])
 
-    assert mo_coeffs[0, 0] == pytest.approx(-1.00154358e+00)
-    assert mo_coeffs[0, 1] == pytest.approx(abs(-2.33624458e-01))
-    assert mo_coeffs[0, 2] == pytest.approx(-4.97111543e-16)
-    assert mo_coeffs[0, 3] == pytest.approx(-8.56842145e-02)
-    assert mo_coeffs[0, 4] == pytest.approx(2.02299681e-29)
-    assert mo_coeffs[0, 5] == pytest.approx(-4.82226067e-02)
-    assert mo_coeffs[0, 6] == pytest.approx(-4.99600361e-16)
+    assert abs(mo_coeffs[0, 0]) == pytest.approx(abs(1.00154358e+00))
+    assert abs(mo_coeffs[0, 1]) == pytest.approx(abs(2.33624458e-01))
+    assert abs(mo_coeffs[0, 2]) == pytest.approx(abs(4.97111543e-16))
+    assert abs(mo_coeffs[0, 3]) == pytest.approx(abs(8.56842145e-02))
+    assert abs(mo_coeffs[0, 4]) == pytest.approx(abs(2.02299681e-29))
+    assert abs(mo_coeffs[0, 5]) == pytest.approx(abs(4.82226067e-02))
+    assert abs(mo_coeffs[0, 6]) == pytest.approx(abs(4.99600361e-16))
     "Solve Roothan Equations Test Failed"
 
 
 def test_form_density_matrix(mol_h2o):
+    Tuv = pickle.load(open("tuv.pkl", "rb"))
+    Vuv = pickle.load(open("vuv.pkl", "rb"))
+    eri = pickle.load(open("eri.pkl", "rb"))
+    Suv = pickle.load(open("suv.pkl", "rb"))
     Duv = SCF.calc_initial_density(mol_h2o)
     h_core = SCF.calc_hcore_matrix(Tuv, Vuv)
     Fuv = SCF.calc_fock_matrix(mol_h2o, h_core, eri, Duv)
@@ -77,6 +90,10 @@ def test_form_density_matrix(mol_h2o):
 
 
 def test_calc_tot_energy(mol_h2o):
+    Tuv = pickle.load(open("tuv.pkl", "rb"))
+    Vuv = pickle.load(open("vuv.pkl", "rb"))
+    eri = pickle.load(open("eri.pkl", "rb"))
+    Suv = pickle.load(open("suv.pkl", "rb"))
     Duv = SCF.calc_initial_density(mol_h2o)
     h_core = SCF.calc_hcore_matrix(Tuv, Vuv)
     Fuv = SCF.calc_fock_matrix(mol_h2o, h_core, eri, Duv)
